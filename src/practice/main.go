3c5e50eb333ed3e36d1a7205cabf21619ad9000e
yuki-tajiri <yuki.tajiri@dena.com>
2018-03-10 15:57:25 +0900

    add git ignore

--- src/practice/main.go
+++ src/practice/main.go
@@ -2,17 +2,13 @@ package main
 
 
 import (
-
 "fmt"
 "net/http"
 
 )
 
 func main(){
-	//　ここでハンドラをとうろくしてエントリーポイントとひもづける
-	http.HandleFunc("/", handler)
-	//　HTTPサーバの利用
-	http.ListenAndServe(":8080", nil)
+
 }
 
 //　レスポンスを書き込む先とクライアントからのリクエストを受け取る
